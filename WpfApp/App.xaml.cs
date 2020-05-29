3b65c0d49686160c06c578c14a6f7008bfbe8a0b
Manoj Choudhari <manoj_choudhari@outlook.com>
2020-05-29 23:38:06 +0200

    Working WPF Version with Azure AD

---
+++ WpfApp/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp.TokenCache;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static string ClientId = "89866898-d443-442e-ad58-21b6b308a520";
+        private static string Tenant = "da41245a5-11b3-996c-00a8-4d99re19f292";
+        private static string Instance = "https://login.microsoftonline.com/";
+
+        public static IPublicClientApplication PublicClientApp { get; private set; }
+
+        static App()
+        {
+            PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
+                .WithAuthority($"{Instance}{Tenant}")
+                .WithRedirectUri("http://localhost")
+                .Build();
+
+            TokenCacheHelper.Bind(PublicClientApp.UserTokenCache);
+        }
+        private static void Log(LogLevel level, string message, bool containsPii)
+        {
+            string logs = ($"{level} {message}");
+            StringBuilder sb = new StringBuilder();
+            sb.Append(logs);
+            File.AppendAllText(System.Reflection.Assembly.GetExecutingAssembly().Location + ".msalLogs.txt", sb.ToString());
+            sb.Clear();
+        }
+    }
+}
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] scopes = new string[] { "user.read" };
+
+            AuthenticationResult authResult = null;
+            var app = App.PublicClientApp;
+            try
+            {
+
+                ResultText.Text = "";
+
+                authResult = await (app as PublicClientApplication).AcquireTokenInteractive(scopes)
+                    .ExecuteAsync();
+
+                DisplayBasicTokenInfo(authResult);
+                UpdateSignInState(true);
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token:{Environment.NewLine}{ex}";
+            }
+        }
+
+        private async void SignOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<IAccount> accounts = await App.PublicClientApp.GetAccountsAsync();
+            try
+            {
+                while (accounts.Any())
+                {
+                    await App.PublicClientApp.RemoveAsync(accounts.FirstOrDefault());
+                    accounts = await App.PublicClientApp.GetAccountsAsync();
+                }
+
+                UpdateSignInState(false);
+            }
+            catch (MsalException ex)
+            {
+                ResultText.Text = $"Error signing-out user: {ex.Message}";
+            }
+        }
+
+        private void UpdateSignInState(bool signedIn)
+        {
+            if (signedIn)
+            {
+                SignOutButton.Visibility = Visibility.Visible;
+                SignInButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ResultText.Text = "";
+                TokenInfoText.Text = "";
+
+                SignOutButton.Visibility = Visibility.Collapsed;
+                SignInButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void DisplayBasicTokenInfo(AuthenticationResult authResult)
+        {
+            TokenInfoText.Text = "";
+            if (authResult != null)
+            {
+                TokenInfoText.Text += $"Username: {authResult.Account.Username}" + Environment.NewLine;
+                TokenInfoText.Text += $"Token Expires: {authResult.ExpiresOn.ToLocalTime()}" + Environment.NewLine;
+            }
+        }
+    }
+}
