f592fcf6c0e8c3080094a4f1ac1d8a442d120948
Manoj Choudhari <manoj_choudhari@outlook.com>
2020-05-30 12:29:59 +0200

    B2C WPF Application, Initial setup

--- B2CWPFApp/App.xaml.cs
+++ B2CWPFApp/App.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using B2CWPFApp.TokenCache;
+using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +17,40 @@ namespace B2CWPFApp
     /// </summary>
     public partial class App : Application
     {
+        private static readonly string Tenant = "fabrikamb2c.onmicrosoft.com";
+        private static readonly string AzureAdB2CHostname = "fabrikamb2c.b2clogin.com";
+        private static readonly string ClientId = "841e1190-d73a-450c-9d68-f5cf16b78e81";
+        private static readonly string RedirectUri = "https://fabrikamb2c.b2clogin.com/oauth2/nativeclient";
+        public static string PolicySignUpSignIn = "b2c_1_susi";
+        public static string PolicyEditProfile = "b2c_1_edit_profile";
+        public static string PolicyResetPassword = "b2c_1_reset";
+
+        public static string[] ApiScopes = { "https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read" };
+        public static string ApiEndpoint = "https://fabrikamb2chello.azurewebsites.net/hello";
+        private static string AuthorityBase = $"https://{AzureAdB2CHostname}/tfp/{Tenant}/";
+        public static string AuthoritySignUpSignIn = $"{AuthorityBase}{PolicySignUpSignIn}";
+        public static string AuthorityEditProfile = $"{AuthorityBase}{PolicyEditProfile}";
+        public static string AuthorityResetPassword = $"{AuthorityBase}{PolicyResetPassword}";
+
+        public static IPublicClientApplication PublicClientApp { get; private set; }
+
+        static App()
+        {
+            PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
+                .WithB2CAuthority(AuthoritySignUpSignIn)
+                .WithRedirectUri(RedirectUri)
+                .WithLogging(Log, LogLevel.Verbose, false) //PiiEnabled set to false
+                .Build();
+
+            TokenCacheHelper.Bind(PublicClientApp.UserTokenCache);
+        }
+        private static void Log(LogLevel level, string message, bool containsPii)
+        {
+            string logs = ($"{level} {message}");
+            StringBuilder sb = new StringBuilder();
+            sb.Append(logs);
+            File.AppendAllText(System.Reflection.Assembly.GetExecutingAssembly().Location + ".msalLogs.txt", sb.ToString());
+            sb.Clear();
+        }
     }
 }
--- B2CWPFApp/MainWindow.xaml.cs
+++ B2CWPFApp/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +13,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,6 +29,258 @@ namespace B2CWPFApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private IAccount GetAccountByPolicy(IEnumerable<IAccount> accounts, string policy)
+        {
+            foreach (var account in accounts)
+            {
+                string accountIdentifier = account.HomeAccountId.ObjectId.Split('.')[0];
+                if (accountIdentifier.EndsWith(policy.ToLower())) return account;
+            }
+
+            return null;
+        }
+
+        private async void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthenticationResult authResult = null;
+            var app = App.PublicClientApp;
+            try
+            {
+                ResultText.Text = "";
+                authResult = await (app as PublicClientApplication).AcquireTokenInteractive(App.ApiScopes)
+                    ////.WithParentActivityOrWindow(new WindowInteropHelper(this).Handle)
+                    .ExecuteAsync();
+
+                DisplayUserInfo(authResult);
+                UpdateSignInState(true);
+            }
+            catch (MsalException ex)
+            {
+                try
+                {
+                    if (ex.Message.Contains("AADB2C90118"))
+                    {
+                        authResult = await (app as PublicClientApplication).AcquireTokenInteractive(App.ApiScopes)
+                            ///.WithParentActivityOrWindow(new WindowInteropHelper(this).Handle)
+                            .WithPrompt(Prompt.SelectAccount)
+                            .WithB2CAuthority(App.AuthorityResetPassword)
+                            .ExecuteAsync();
+                    }
+                    else
+                    {
+                        ResultText.Text = $"Error Acquiring Token:{Environment.NewLine}{ex}";
+                    }
+                }
+                catch (Exception exe)
+                {
+                    ResultText.Text = $"Error Acquiring Token:{Environment.NewLine}{exe}";
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token:{Environment.NewLine}{ex}";
+            }
+        }
+
+        private async void EditProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var app = App.PublicClientApp;
+            try
+            {
+                ResultText.Text = $"Calling API:{App.AuthorityEditProfile}";
+
+                AuthenticationResult authResult = await (app as PublicClientApplication).AcquireTokenInteractive(App.ApiScopes)
+                            ////.WithParentActivityOrWindow(new WindowInteropHelper(this).Handle)
+                            .WithB2CAuthority(App.AuthorityEditProfile)
+                            .WithPrompt(Prompt.NoPrompt)
+                            .ExecuteAsync(new System.Threading.CancellationToken());
+
+                DisplayUserInfo(authResult);
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Session has expired, please sign out and back in.{App.AuthorityEditProfile}{Environment.NewLine}{ex}";
+            }
+        }
+
+        private async void CallApiButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthenticationResult authResult = null;
+            var app = App.PublicClientApp;
+            IEnumerable<IAccount> accounts = await App.PublicClientApp.GetAccountsAsync();
+            try
+            {
+                authResult = await app.AcquireTokenSilent(App.ApiScopes, GetAccountByPolicy(accounts, App.PolicySignUpSignIn))
+                    .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilentAsync. 
+                // This indicates you need to call AcquireTokenAsync to acquire a token
+                Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+                try
+                {
+                    authResult = await app.AcquireTokenInteractive(App.ApiScopes)
+                        ////.WithParentActivityOrWindow(new WindowInteropHelper(this).Handle)
+                        .ExecuteAsync();
+                }
+                catch (MsalException msalex)
+                {
+                    ResultText.Text = $"Error Acquiring Token:{Environment.NewLine}{msalex}";
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token Silently:{Environment.NewLine}{ex}";
+                return;
+            }
+
+            if (authResult != null)
+            {
+                if (string.IsNullOrEmpty(authResult.AccessToken))
+                {
+                    ResultText.Text = "Access token is null (could be expired). Please do interactive log-in again.";
+                }
+                else
+                {
+                    ResultText.Text = await GetHttpContentWithToken(App.ApiEndpoint, authResult.AccessToken);
+                    DisplayUserInfo(authResult);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Perform an HTTP GET request to a URL using an HTTP Authorization header
+        /// </summary>
+        /// <param name="url">The URL</param>
+        /// <param name="token">The token</param>
+        /// <returns>String containing the results of the GET operation</returns>
+        public async Task<string> GetHttpContentWithToken(string url, string token)
+        {
+            var httpClient = new HttpClient();
+            HttpResponseMessage response;
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                response = await httpClient.SendAsync(request);
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        private async void SignOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<IAccount> accounts = await App.PublicClientApp.GetAccountsAsync();
+            try
+            {
+                while (accounts.Any())
+                {
+                    await App.PublicClientApp.RemoveAsync(accounts.FirstOrDefault());
+                    accounts = await App.PublicClientApp.GetAccountsAsync();
+                }
+
+                UpdateSignInState(false);
+            }
+            catch (MsalException ex)
+            {
+                ResultText.Text = $"Error signing-out user: {ex.Message}";
+            }
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var app = App.PublicClientApp;
+                IEnumerable<IAccount> accounts = await App.PublicClientApp.GetAccountsAsync();
+
+                AuthenticationResult authResult = await app.AcquireTokenSilent(App.ApiScopes,
+                                                                               GetAccountByPolicy(accounts, App.PolicySignUpSignIn))
+                    .ExecuteAsync();
+
+                DisplayUserInfo(authResult);
+                UpdateSignInState(true);
+            }
+            catch (MsalUiRequiredException)
+            {
+                // Ignore, user will need to sign in interactively.
+                ResultText.Text = "You need to sign-in first, and then Call API";
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token Silently:{Environment.NewLine}{ex}";
+            }
+        }
+
+        private void UpdateSignInState(bool signedIn)
+        {
+            if (signedIn)
+            {
+                CallApiButton.Visibility = Visibility.Visible;
+                EditProfileButton.Visibility = Visibility.Visible;
+                SignOutButton.Visibility = Visibility.Visible;
+
+                SignInButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ResultText.Text = "";
+                TokenInfoText.Text = "";
+
+                CallApiButton.Visibility = Visibility.Collapsed;
+                EditProfileButton.Visibility = Visibility.Collapsed;
+                SignOutButton.Visibility = Visibility.Collapsed;
+
+                SignInButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void DisplayUserInfo(AuthenticationResult authResult)
+        {
+            TokenInfoText.Text = "";
+            if (authResult != null)
+            {
+                JObject user = ParseIdToken(authResult.IdToken);
+
+                TokenInfoText.Text += $"Name: {user["name"]?.ToString()}" + Environment.NewLine;
+                TokenInfoText.Text += $"User Identifier: {user["oid"]?.ToString()}" + Environment.NewLine;
+                TokenInfoText.Text += $"Street Address: {user["streetAddress"]?.ToString()}" + Environment.NewLine;
+                TokenInfoText.Text += $"City: {user["city"]?.ToString()}" + Environment.NewLine;
+                TokenInfoText.Text += $"State: {user["state"]?.ToString()}" + Environment.NewLine;
+                TokenInfoText.Text += $"Country: {user["country"]?.ToString()}" + Environment.NewLine;
+                TokenInfoText.Text += $"Job Title: {user["jobTitle"]?.ToString()}" + Environment.NewLine;
+
+                if (user["emails"] is JArray emails)
+                {
+                    TokenInfoText.Text += $"Emails: {emails[0].ToString()}" + Environment.NewLine;
+                }
+                TokenInfoText.Text += $"Identity Provider: {user["iss"]?.ToString()}" + Environment.NewLine;
+            }
+        }
+
+        JObject ParseIdToken(string idToken)
+        {
+            // Parse the idToken to get user info
+            idToken = idToken.Split('.')[1];
+            idToken = Base64UrlDecode(idToken);
+            return JObject.Parse(idToken);
+        }
+
+        private string Base64UrlDecode(string s)
+        {
+            s = s.Replace('-', '+').Replace('_', '/');
+            s = s.PadRight(s.Length + (4 - s.Length % 4) % 4, '=');
+            var byteArray = Convert.FromBase64String(s);
+            var decoded = Encoding.UTF8.GetString(byteArray, 0, byteArray.Count());
+            return decoded;
         }
     }
 }
